0368cc6f4523654c5d560cc08e8251f50d69b11a
Freedom Loisa <kitakayaloisa@gmail.com>
2023-08-04 17:41:58 +0300

    App changes

--- home/views.py
+++ home/views.py
@@ -1,8 +1,7 @@
-from extensions import db
-from sqlalchemy import and_
 from datetime import datetime
 from models.images import Image
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 bp = Blueprint("home", __name__)
 
@@ -10,13 +9,20 @@ bp = Blueprint("home", __name__)
 @bp.route("/", methods=["GET"])
 def index():
     today = datetime.now().timestamp()
-    start_date = datetime(2023, 8, 15).timestamp()
+    start_date = datetime(2023, 8, 1).timestamp()
 
     approved_images = []
+    approved_length = 0
 
     if start_date <= today:
-        approved_images = Image.query.filter_by(approved=True).all()
+        if current_user.is_authenticated:
+            approved_images = Image.query.all()
+            approved_length = len(approved_images)
+
+        else:
+            approved_images = Image.query.filter_by(approved=True).all()
+            approved_length = len(approved_images)
 
     print(approved_images)
 
-    return render_template("index.html", images=approved_images)
+    return render_template("index.html", images=approved_images, length=approved_length)
